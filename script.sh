9e676e67d90d0990a8a4d64cd738c20af79b3a3b
Annavar satish <annavarsatish10@gmail.com>
2023-07-18 19:14:53 +0530

    updated the script and added clusterrole

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@ until kubectl get nodes | grep 'NotReady'
 do
   echo 'Deleting the Not Ready node'
   node_id=$(kubectl get nodes | grep 'NotReady' | awk '{print $1}')
+  kubectl drain $node_id --force --ignore-daemonsets --delete-emptydir-data
   kubectl delete node $node_id
-  sleep 5
 done
