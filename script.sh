e721e5677e4ff19e2f4b9fd28999ffa55c3d7127
Annavar satish <annavarsatish10@gmail.com>
2023-07-19 15:35:39 +0530

    moved daemonset to cronjob and whileloop

--- script.sh
+++ script.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-until kubectl get nodes | grep 'NotReady'
+while kubectl get nodes | grep 'NotReady'
 do
   echo 'Deleting the Not Ready node'
   node_id=$(kubectl get nodes | grep 'NotReady' | awk '{print $1}')
-  kubectl drain $node_id --force --ignore-daemonsets --delete-emptydir-data
   kubectl delete node $node_id
 done
