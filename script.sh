cbfbd20f22815af1902a5a0db765679d229f5406
Annavar satish <annavarsatish10@gmail.com>
2024-01-05 16:15:38 +0530

    Add design file

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 while kubectl get nodes | grep 'NotReady'
 do
+  sleep 30
   echo 'Deleting the Not Ready node'
   node_id=$(kubectl get nodes | grep 'NotReady' | awk '{print $1}')
   kubectl delete node $node_id
